e638958c2c75eb552e074e3f82e41c68571604bd
Joshhortt <joshhortt@yahoo.com>
2022-12-17 12:56:22 +0000

    adding Links to initial SPA (single page app)

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,16 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
-  return <h1>Olá Rastrum!</h1>;
+  return (
+    <main id="content">
+      <h1>A melhor forma de manter um registo das suas notas!</h1>
+      <p>
+        Experimente a nossa versão Beta e nunca perca o controle das suas
+        despesas
+      </p>
+      <p id="cta">
+        <Link to="/notes">Experimenta agora!</Link>
+      </p>
+    </main>
+  );
 }
---
+++ app/routes/notes.jsx
@@ -0,0 +1,7 @@
+export default function NotesPage() {
+  return (
+    <main>
+      <h1>As minhas Notas</h1>;
+    </main>
+  );
+}
