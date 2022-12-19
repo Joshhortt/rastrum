2612a1a76efecdaa4ff41622049904f9effd72c7
Joshhortt <joshhortt@yahoo.com>
2022-12-19 16:49:35 +0000

    add NoteList: fetch & return page data via Loader

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,20 +1,30 @@
 import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import NoteList, { links as noteListLinks } from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from "~/data/notes";
-// import newNoteStyles from "~/components/NewNote.css";
 
 export default function NotesPage() {
+  const notes = useLoaderData();
+
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   );
+}
+
+export async function loader() {
+  const notes = await getStoredNotes();
+  return notes;
 }
 
 export async function action({ request }) {
   const formData = await request.formData();
   const noteData = Object.fromEntries(formData);
-  // Add validation here
+  // Add validation...
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
   const updatedNotes = existingNotes.concat(noteData);
@@ -23,5 +33,5 @@ export async function action({ request }) {
 }
 
 export function links() {
-  return [...newNoteLinks()];
+  return [...newNoteLinks(), ...noteListLinks()];
 }
